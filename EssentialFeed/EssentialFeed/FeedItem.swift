2e121cef086cefdb39e944c152114167c7aea9ba
aaronmhr <aaronmhr@gmail.com>
2020-09-11 13:14:29 +0200

    Create the `FeedItem` model

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Aaron Huánuco on 11/09/2020.
+//  Copyright © 2020 Aaron Huánuco. All rights reserved.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
