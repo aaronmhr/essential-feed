9b227bc12a72db2363ec4c0fd6e86830c7397a0c
aaronmhr <aaronmhr@gmail.com>
2020-09-17 23:36:04 +0200

    Add `FeedStoreSpecs`

---
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -0,0 +1,126 @@
+//
+//  CoreDataFeedStoreTests.swift
+//  EssentialFeedTests
+//
+//  Created by Aaron Huánuco on 17/09/2020.
+//  Copyright © 2020 Aaron Huánuco. All rights reserved.
+//
+
+import XCTest
+import EssentialFeed
+
+final class CoreDataFeedStoreTests: XCTestCase, FailableFeedStoreSpecs {
+    func test_retrieve_deliversEmptyOnEmptyCache() {
+//        let sut = makeSUT()
+//
+//        assertThatRetrieveDeliversEmptyOnEmptyCache(on: sut)
+    }
+
+    func test_retrieve_hasNoSideEffectsOnEmptyCache() {
+//        let sut = makeSUT()
+//
+//        assertThatRetrieveHasNoSideEffectsOnEmptyCache(on: sut)
+    }
+
+    func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
+//        let sut = makeSUT()
+//
+//        assertThatRetrieveDeliversFoundValuesOnNonEmptyCache(on: sut)
+    }
+
+    func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
+//        let sut = makeSUT()
+//
+//        assertThatRetrieveHasNoSideEffectsOnEmptyCache(on: sut)
+    }
+
+    func test_retrieve_deliversFailureOnRetrievalError() {
+//        // MARK: Adapt to CoreData
+//        let sut = makeSUT()
+//
+//        assertThatRetrieveDeliversFailureOnRetrievalError(on: sut)
+    }
+
+    func test_retrieve_hasNoSideEffectsOnFailure() {
+//        // MARK: Adapt to CoreData
+//        let sut = makeSUT()
+//
+//        assertThatRetrieveHasNoSideEffectsOnFailure(on: sut)
+    }
+
+    func test_insert_deliversNoErrorOnEmptyCache() {
+//        let sut = makeSUT()
+//
+//        assertThatInsertDeliversNoErrorOnEmptyCache(on: sut)
+    }
+
+    func test_insert_deliversNoErrorOnNonEmptyCache() {
+//        let sut = makeSUT()
+//
+//        assertThatInsertDeliversNoErrorOnNonEmptyCache(on: sut)
+    }
+
+    func test_insert_overridesPreviouslyInsertedCacheValues() {
+//        let sut = makeSUT()
+//
+//        assertThatInsertOverridesPreviouslyInsertedCacheValues(on: sut)
+    }
+
+    func test_insert_deliversErrorOnInsertionError() {
+//        let invalidStoreURL = URL(string: "invalid://store-url")!
+//        let sut = makeSUT(storeURL: invalidStoreURL)
+//
+//        assertThatInsertDeliversErrorOnInsertionError(on: sut)
+    }
+
+    func test_insert_hasNoSideEffectsOnInsertionError() {
+//        let invalidStoreURL = URL(string: "invalid://store-url")!
+//        let sut = makeSUT(storeURL: invalidStoreURL)
+//
+//        assertThatInsertHasNoSideEffectsOnInsertionError(on: sut)
+    }
+
+    func test_delete_deliversNoErrorOnEmptyCache() {
+//        let sut = makeSUT()
+//
+//        assertThatDeleteDeliversNoErrorOnEmptyCache(on: sut)
+    }
+
+    func test_delete_hasNoSideEffectsOnEmptyCache() {
+//        let sut = makeSUT()
+//
+//        assertThatDeleteHasNoSideEffectsOnEmptyCache(on: sut)
+    }
+
+    func test_delete_deliversNoErrorOnNonEmptyCache() {
+//        let sut = makeSUT()
+//
+//        assertThatDeleteDeliversNoErrorOnNonEmptyCache(on: sut)
+    }
+
+    func test_delete_emptiesPreviouslyInsertedCache() {
+//        let sut = makeSUT()
+//
+//        assertThatDeleteEmptiesPreviouslyInsertedCache(on: sut)
+    }
+
+    func test_delete_deliversErrorOnDeletionError() {
+//        // MARK: Adapt to CoreData
+//        let sut = makeSUT()
+//
+//        assertThatDeleteDeliversErrorOnDeletionError(on: sut)
+    }
+
+    func test_delete_hasNoSideEffectsOnDeletionError() {
+//        // MARK: Adapt to CoreData
+//        let sut = makeSUT()
+//
+//        assertThatDeleteHasNoSideEffectsOnDeletionError(on: sut)
+    }
+
+    func test_storeSideEffects_runSerially() {
+//        let sut = makeSUT()
+//
+//        assertThatSideEffectsRunSerially(on: sut)
+    }
+}
