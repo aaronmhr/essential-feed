a5e88d089d1c2dd239d41c1bf6c2e3e08c6b22ff
aaronmhr <aaronmhr@gmail.com>
2020-09-19 23:39:50 +0200

    Add padding animation to simulate asynchronous loading

--- Prototype/Prototype/FeedImageCell.swift
+++ Prototype/Prototype/FeedImageCell.swift
@@ -12,6 +12,30 @@ final class FeedImageCell: UITableViewCell {
     @IBOutlet private(set) var locationLabel: UILabel!
     @IBOutlet private(set) var feedImageView: UIImageView!
     @IBOutlet private(set) var descriptionLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        feedImageView.alpha = 0
+    }
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+
+        feedImageView.alpha = 0
+    }
+
+    func fadeIn(_ image: UIImage?) {
+        feedImageView.image = image
+
+        UIView.animate(
+            withDuration: 0.3,
+            delay: 0.3,
+            options: [],
+            animations: {
+                self.feedImageView.alpha = 1
+            })
+    }
 }
 
 extension FeedImageCell {
@@ -22,6 +46,6 @@ extension FeedImageCell {
         descriptionLabel.text = model.description
         descriptionLabel.isHidden = model.description == nil
 
-        feedImageView.image = UIImage(named: model.imageName)
+        fadeIn(UIImage(named: model.imageName))
     }
 }
